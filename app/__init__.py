4b081bbab2c73437d2ebefec07afdf756f55f01a
Pablo Ortega <portega704@gmail.com>
2025-03-22 12:01:50 +0100

    change to GET

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-app = Flask(__name__)
 
+
+
+app = Flask(__name__)
 CORS(app)
 
 from app import routes
-
 
 
--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,6 @@ def hello_world():
 
 
 
-# Security: API Key Authentication
-VALID_API_KEYS = {
-    "user1": "key_ABC123",
-    "user2": "key_XYZ789",
-    "admin": "key_SUPERSECURE"
-}
 CSV_FILE = "data/loads.csv"  # Path to your CSV file
 
 def find_load(reference_number):
@@ -30,29 +24,8 @@ def find_load(reference_number):
         return None
     return None
 
-'''
-@app.before_request
-def check_api_key():
-    # Get the Authorization header (e.g., "Bearer key_ABC123")
-    auth_header = request.headers.get("Authorization")
-    print(auth_header)
-    
-    # If no Authorization header is provided, return an error
-    if not auth_header:
-        return jsonify({"error": "Missing API key"}), 400
-    
-    # Extract the API key from the "Bearer" prefix
-    token = auth_header.split(" ")[1] if len(auth_header.split(" ")) > 1 else None
 
-    if not token:
-        return jsonify({"error": "Invalid API key format"}), 400
-    
-    # Validate the API key
-    if token not in VALID_API_KEYS:
-        return jsonify({"error": "Unauthorized access"}), 401
-
-'''
-@app.route("/loads", methods=["POST"])
+@app.route("/loads", methods=["GET"])
 def get_load():
     # Get the JSON data from the request
     data = request.get_json()
@@ -68,4 +41,4 @@ def get_load():
 
 
 if __name__ == "__main__":
-    app.run() #ssl_context=('cert.pem', 'key.pem'))  # Enforce HTTPS
+    app.run() 
